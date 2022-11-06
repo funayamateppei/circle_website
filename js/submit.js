a6f83851fd8f565f02cb9b26f4ec3d369b6c2c72
funayamateppei <t.funayama0217@gmail.com>
2022-11-06 12:18:17 +0900

    firebase

--- js/submit.js
+++ js/submit.js
@@ -1,25 +1,25 @@
 
 
 
-let newPostRef = firebase.database().ref();
+// let newPostRef = firebase.database().ref();
 
-const data = [];
+// const data = [];
 
-$('#send').on('click', () => {
-  // データを持ってきておく必要がある databaseから
-  const obj = {
-    college: $('#college').val(),
-    circleName: $('#circleName').val(),
-    text: $('#sentence').val()
-  }
-// 大学名に一致がなければ追加しない
-  data.push(obj);
+// $('#send').on('click', () => {
+//   // データを持ってきておく必要がある databaseから
+//   const obj = {
+//     college: $('#college').val(),
+//     circleName: $('#circleName').val(),
+//     text: $('#sentence').val()
+//   }
+// // 大学名に一致がなければ追加しない
+//   data.push(obj);
 
-  console.log(data);
+//   console.log(data);
 
-  newPostRef.push(data)
+//   newPostRef.push(data)
   
-  // const json = JSON.stringify(data);
-  // localStorage.setItem('aaa', json);
-  // Firebaseにデータを送る
-})
+//   // const json = JSON.stringify(data);
+//   // localStorage.setItem('aaa', json);
+//   // Firebaseにデータを送る
+// })
