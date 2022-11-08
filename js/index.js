a24e3f4ffa85f514d13e1f74f513087ad6d06965
funayamateppei <t.funayama0217@gmail.com>
2022-11-08 22:29:45 +0900

    html

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 // ------------------- hamburger menu -----------------------
-const bar = document.querySelector('.navbar');
-const p = document.querySelector('.nav p');
-const login = document.querySelector('.loginDisplay');
+// const bar = document.querySelector('.navbar');
+// const p = document.querySelector('.nav p');
+// const login = document.querySelector('.loginDisplay');
 
 
-bar.addEventListener('click', () => {
-  login.classList.toggle('list-active');
-  bar.classList.toggle('toggle');
-  p.classList.toggle('none');
-});
+// bar.addEventListener('click', () => {
+//   login.classList.toggle('list-active');
+//   bar.classList.toggle('toggle');
+//   p.classList.toggle('none');
+// });
 
 
 // ------------------------- main -------------------------------
