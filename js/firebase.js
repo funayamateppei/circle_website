f406a9bd52a144d991d5a8f393539ce7adbe17d6
funayamateppei <t.funayama0217@gmail.com>
2022-11-10 18:15:58 +0900

    add

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,37 @@
 import {
-  initializeApp
+  initializeApp,
+  // firebase,
+  // getapp
 } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
 
 import {
   getAuth,
   signOut,
+  onAuthStateChanged,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
 
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  setDoc,
+  query,
+  where,
+  doc,
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js"
+
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAL8_-e9Q7xvWLiwRr6vC9-9oOSImNtGeI",
+  apiKey: "",
   authDomain: "club-website-ad955.firebaseapp.com",
   projectId: "club-website-ad955",
   storageBucket: "club-website-ad955.appspot.com",
@@ -20,16 +41,19 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app)
+const storageRef = ref(storage, 'club-image');
 
-
-// 新規登録の処理
+// -------------------------------------------------- 新規登録の処理
 $('#signupBtn').on('click', () => {
+  const username = $('#signupName').val();
   const email = $('#signupMail').val();
   const password = $('#signupPass').val();
   const password2 = $('#signupPass2').val();
   // 入力内容の漏れ確認
-  if (email === '' || password === '') {
-    $('.error').text('メールアドレス、パスワードを入力してください');
+  if (username === '' || email === '' || password === '') {
+    $('.error').text('ユーザー名、メールアドレス、パスワードを入力してください');
     return
   }
   // パスワード確認
@@ -41,15 +65,19 @@ $('#signupBtn').on('click', () => {
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // 登録成功後に起こる処理
-      // const user = userCredential.user;
-      // console.log(user);
+      const user = userCredential.user;
+      // Firestoreにユーザー名、メールアドレス、uidを保存する
+      addDoc(collection(db, 'users'), {
+        username: username,
+        email: user.email,
+        uid: user.uid
+      })
       alert('登録が完了しました。')
       $('#signupMail').val('');
       $('#signupPass').val('');
       $('.signup').hide();
-      $('.top').hide();
-      $('.home').fadeIn();
-      $('header').fadeIn();
+      $('.login').fadeIn();
+      $('.error').text('');
     })
     .catch((error) => {
       const errorCode = error.code;
@@ -64,11 +92,12 @@ $('#signupBtn').on('click', () => {
       }
       // console.log(errorCode); // if文の中でエラーコードを使用するため呼んだ
     });
+  
 });
 
 
 
-// ログインの処理
+// ---------------------------------------------------- ログインの処理
 $('#loginBtn').on('click', () => {
   const email = $('#loginMail').val();
   const password = $('#loginPass').val();
@@ -77,7 +106,8 @@ $('#loginBtn').on('click', () => {
   }
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      const user = userCredential.user;
+      // const user = userCredential.user;
+      // console.log(user.email);
       // ログイン成功時に起こる処理
       $('#loginMail').val('');
       $('#loginPass').val('');
@@ -85,6 +115,7 @@ $('#loginBtn').on('click', () => {
       $('.top').hide();
       $('.home').fadeIn();
       $('header').fadeIn();
+      $('.error').text('');
     })
     .catch((error) => {
       const errorCode = error.code;
@@ -101,6 +132,12 @@ $('#loginBtn').on('click', () => {
         return
       }
     });
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      console.log(uid);
+    } 
+  });
 })
 
 // ログアウトの処理
@@ -118,3 +155,53 @@ $('#logout').on('click', () => {
       alert(error);
     })
 })
+
+let documentId = '';
+// firestoreに情報追加
+$('.submit').on('click', () => {
+  // フォームの内容をfirestoreに保存する
+  // storageに画像保存
+  const files = document.getElementById('imgForm').files
+  console.log(files[0]); // 確認しとけい
+  const imgfiles = ref(storageRef, files[0].name);
+  uploadBytes(imgfiles, files[0])
+    .then((snapshot) => {
+      // storageに保存した画像のURLをもってくる imageタグのsrc属性で使うため
+      getDownloadURL(imgfiles).then((url) => {
+        console.log(url);
+        const imgURL = url; // 定数に入れておく
+        // userのuidが欲しいから、今ログインしているuserの状況を取得する
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            const uid = user.uid;
+            console.log(uid);
+            // 取得したuidとusersコレクションの中のuidプロパティが一致するドキュメントID取得
+            getDocs(query((collection(db, 'users')), where('uid', '==', uid)))
+              .then(snapshot => {
+                console.log(snapshot);
+                snapshot.forEach(doc => {   // ここがなんで繰り返してんのかまじわからんゴミ
+                  console.log(`${doc.id}`);
+                  // グローバルで定義した空のdocumentIdに戻り値として渡す
+                  return documentId = doc.id; 
+                })
+                // 取得したドキュメントIDを指定して中身にdataを追加する
+                // 第三引数にmerge: trueを入れることで元からあったものが消えない
+                // updateDoc()でもよかった？わからんからこっち使った！
+                setDoc(doc(db, 'users', documentId), {
+                  univ: $('#univName').val(),
+                  club: $('#clubName').val(),
+                  text: $('#text').val(),
+                  img: imgURL,  // さっき取得したstorageに保存した画像のURL
+                  insta: $('#insta').val(),
+                  twitter: $('#twitter').val()
+                }, { merge: true })
+              })
+          } 
+        });
+      })
+      $('#imgForm').val('')
+    });
+});
+
+
+// マイページへ移動したときに起きる処理（ログインユーザーに登録してある情報を表示）
--- readme.md
+++ readme.md
@@ -3,9 +3,29 @@
 # 参考にしたサイト
 - ログイン機能（JSとFirebase）
   - https://firebase.google.com/docs/auth/web/start?authuser=0&hl=ja
+  - https://www.azukipan.com/posts/firebase-authentication-logout/
+
+- Firestore 大まかな説明 公式ドキュメントの方がわかりやすいけど一応
+  - https://qiita.com/azukiazusa/items/2cb7405c47ac38fa3fd1
 
 - Firebase Auth エラー内容
   - https://qiita.com/thoth000/items/5b31e5f1daf7c2284473
+
+- Firebase Storage 画像保存方法
+  - https://firebase.google.com/docs/storage/web/upload-files?hl=ja
+  - https://norm-nois.com/blog/archives/4080
+
+- Firebase クエリまとめサイト
+  - https://zenn.dev/tentel/articles/ea7d5c03e68e6d142d98
+
+- ログインした人だけ情報を公開できる。ログインしていない人は見るだけ
+  - https://qiita.com/karayok/items/0996c8f0ea219c284dbd
+
+- Firestore where() の使い方
+  - https://tedate.jp/firebase/basic-data-acquisition-methods-from-cloud-firestore
+
+- 非同期処理
+  - https://knowledge.sakura.ad.jp/24890/#then_catch
 
 - いいかんじに画像のサイズを調整してくれるcss object-fit: cover;
   - https://www.webcreatorbox.com/tech/object-fit
@@ -15,3 +35,9 @@
 
 - input要素でファイルを入れる方法 input type=file
   - https://code-kitchen.dev/html/input-file/
+
+- instagram background-color
+  - https://stackoverflow.com/questions/37751375/instagram-new-logo-css-background
+
+- aタグ アンダーライン表示しない
+  - https://toretama.jp/link-underline-none.html#:~:text=%E4%B8%80%E9%83%A8%E3%81%AE%E3%83%AA%E3%83%B3%E3%82%AF%E3%81%AE%E3%82%A2%E3%83%B3%E3%83%80%E3%83%BC%E3%83%A9%E3%82%A4%E3%83%B3%E3%82%92%E6%B6%88%E3%81%99%E3%81%AB,%E3%81%A6%E3%81%8A%E3%81%84%E3%81%A6%E3%81%8F%E3%81%A0%E3%81%95%E3%81%84%E3%81%AD%E3%80%82
